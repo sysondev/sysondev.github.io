3f61b0d694d340860ef28f74216678cc0532e803
Soroush Hakami <soroush.hakami@sj.se>
2015-09-05 14:36:10 +0200

    fetch instagram feed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 ready(function(){
-    var nav = responsiveNav(".nav-collapse");
+    var nav = responsiveNav('.nav-collapse');
+
+    getRequest('https://api.instagram.com/v1/users/1805388781/media/recent/?client_id=6f84264e1db54f31b62a2a823557f7f3',
+    function() {
+        console.log(this.response);
+    })
 });
 
 function ready(fn) {
@@ -8,4 +13,11 @@ function ready(fn) {
     } else {
         document.addEventListener('DOMContentLoaded', fn);
     }
+}
+
+function getRequest(url, callback) {
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', callback);
+    oReq.open('GET', url, true);
+    oReq.send();
 }
