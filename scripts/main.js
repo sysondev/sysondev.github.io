f4330b81ee50c54aa89445adc2e2fae29be07e44
Love Gehlin <love.gehlin@gmail.com>
2015-09-12 15:53:53 +0200

    testing landing image

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 ready(function(){
+  var s = skrollr.init({
+       render: function(data) {
+           //Debugging - Log the current scroll position.
+           //console.log(data.curTop);
+       }
+   });
 });
 
 function ready(fn) {
