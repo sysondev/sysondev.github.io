e4f9148be678b866f9e16dec4a2f9ded3d942a29
Soroush Hakami <soroush.hakami@sj.se>
2015-09-17 19:29:51 +0200

    removing skrollr

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,4 @@
 ready(function(){
-  var s = skrollr.init({
-       render: function(data) {
-           //Debugging - Log the current scroll position.
-           //console.log(data.curTop);
-       }
-   });
 });
 
 function ready(fn) {
